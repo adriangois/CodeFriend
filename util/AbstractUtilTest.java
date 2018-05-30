dc36e80dad17b8fff05ea4172fef2d68e5cb2205
adrian <contato@abgsolucoes.com.br>
2018-05-30 16:27:38 -0300

    Inicial

---
+++ util/AbstractUtilTest.java
@@ -0,0 +1,41 @@
+package br.com.abgsolucoes.sigaweb.tests.util;
+
+
+import org.junit.Test;
+
+
+
+//@RunWith(Junit4ClassRunner.class)
+//@ContextConfiguration(locations = {  })
+//@Transactional
+/*@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
+		DirtiesContextTestExecutionListener.class,
+		TransactionalTestExecutionListener.class,
+		DbUnitTestExecutionListener.class })
+*/
+public class AbstractUtilTest /*extends DBTestCase*/ {
+
+	@Test
+	public void init() {
+
+	}
+
+/*	@Override
+	protected IDataSet getDataSet() throws Exception {
+		return null;
+	}
+
+	@Override
+	protected void setUpDatabaseConfig(DatabaseConfig config)
+
+	{*/
+		/*
+		 * config.setProperty(DatabaseConfig.PROPERTY_DATATYPE_FACTORY, new
+		 * MysqlDataSourceFactory());
+		 */
+	//}
+	
+		
+
+
+}
